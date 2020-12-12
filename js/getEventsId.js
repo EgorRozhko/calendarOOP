526617130a700d54565401e6c6cdb61c24ada8a4
EgorRozhko <59769440+EgorRozhko@users.noreply.github.com>
2020-12-12 19:16:21 +0300

    Update getEventsId.js

--- js/getEventsId.js
+++ js/getEventsId.js
@@ -6,14 +6,15 @@ $(document).ready(function(){
 			type: "POST",
 			data: ({i_id: item_id}),
 			dataType: "text",
-			success: func_success
+			success: function(data){
+				$('map, .bigCalendar, .monument, .obla2').remove();
+				$('.activeYear').removeClass('activeYear');
+				$('#map').attr('src','images/blockedObla.png');
+				$('#map-container').append(data);
+				$('body, html').animate({scrollTop: $('.obla').offset().top }, 1500);
+				$('.obla').addClass('b');
+			}
 		});
-		$('#listOfEvents').empty();
-		$('#listOfEvents').css({
-			display:'none'
-		});
-		$(".bigCalendar").remove();
-		$('.obla2').remove(); 
 	});
 
 	$('body').on('click','.obla',function(){
@@ -23,7 +24,7 @@ $(document).ready(function(){
 			type: "POST",
 			data: ({i_id: item_id}),
 			dataType: "text",
-			success: func_success
+			success: function(data){ $('body').append(data); }
 		});
 	});
 
@@ -33,13 +34,4 @@ $(document).ready(function(){
 	$('body').on('click','.readMore', function(){
 		$(location).attr('href','article.php?a='+this.id+'-f-a');
 	});
-
-	function func_success(data)
-	{
-		$('area').remove();
-		$('.obla').remove();
-		$('#map-container').append(data);
-		$('body, html').animate({scrollTop: $('.obla').offset().top }, 1500);
-		$('.obla').addClass('b');
-	};
 });
