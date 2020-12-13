eb7de6c3a04ebe7aab5297f2b1d6f517befd16f3
EgorRozhko <59769440+EgorRozhko@users.noreply.github.com>
2020-12-13 15:59:32 +0300

    Update search.php

--- php/search.php
+++ php/search.php
@@ -2,7 +2,7 @@
 	$search_text = $_POST['searchText'];
 	$search_text = trim($search_text);
 	$search_text = htmlspecialchars($search_text);
-	$search_string = 'SELECT events.events_id,events.title,place_id,map_image,coords FROM places INNER JOIN events ON events.place_id = places.id_place WHERE events.metadata LIKE "%'.$search_text.'%"';
+	$search_string = 'SELECT events.events_id,events.title,place_id,map_image,coords FROM places INNER JOIN events ON events.place_id = places.id_place WHERE events.short_description LIKE "%'.$search_text.'%"';
 	if (!empty($search_text))
 	{
 		if (strlen($search_text)>128) echo 'Слишком длинный запрос';
@@ -12,7 +12,7 @@
 			$query = mysqli_query($connection, $search_string);
 			echo "
 				<div id='foundEvents'>";
-			while($res = mysqli_fetch_array($query)) echo "<p id=".$res['id']." class='result'>".$res['title']."</p>";
+			while($res = mysqli_fetch_array($query)) echo "<p id=".$res['events_id']." class='result'>".$res['title']."</p>";
 			echo "</div>";
 		}
 	}
