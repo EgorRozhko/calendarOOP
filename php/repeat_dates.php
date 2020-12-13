5d0ee6ce7b3fa40c0af209942b53d936a5f19b8e
EgorRozhko <59769440+EgorRozhko@users.noreply.github.com>
2020-12-13 15:02:27 +0300

    Update repeat_dates.php

--- php/repeat_dates.php
+++ php/repeat_dates.php
@@ -1,10 +1,14 @@
 <?php
 	include_once '../mysql/connect.php';
-	$res = mysqli_query($connection,'SELECT events_id,title FROM events WHERE date_event=(SELECT date_event FROM events WHERE events_id='.$_POST['i_id'].')');
+	$res = mysqli_query($connection,'SELECT events_id,title FROM events WHERE date_event = "'.$_POST['i_id'].'"');
 	echo "<div id='listOfEvents'>";
- 	while ($row = mysqli_fetch_array($res))
- 	echo "<p class='oneOfEvents' id=".$row['events_id'].">".$row['title']."</p></center>";
- 	echo "<p class='addEvent'>Добавить событие</p>";
+ 	if (mysqli_num_rows($res) > 0)
+ 	{
+ 		while ($row = mysqli_fetch_array($res))
+ 		echo "<p class='oneOfEvents' id=".$row['events_id'].">".$row['title']."</p></center>";
+ 	}
+ 	else echo'<p id="no_event">В этот день нет значимых событий.</p>';
+ 	echo "<p class='addEvent'>Добавить событие</p></div>";
 	mysqli_free_result($res);
 	mysqli_close($connection);
 ?>
