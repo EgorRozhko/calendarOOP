e49da908ce023f57a8fba1a94236c65fbb111ae4
EgorRozhko <59769440+EgorRozhko@users.noreply.github.com>
2020-12-13 15:58:24 +0300

    Update getEvents.js

--- js/getEvents.js
+++ js/getEvents.js
@@ -1,21 +1,24 @@
 $('document').ready(function(){
-	var item_id;
 	var elem;
 	var y;
 	var x;
+	var date;
 	$('body').on('click','.b-calendar__day',function(){
 		$('#listOfEvents').remove();
-		item_id = this.id;
 		elem = this.getBoundingClientRect();
 		x = elem.x;
 		y = elem.y;
+		date = $(this).attr('value');
 		$.ajax({
 			url: "php/repeat_dates.php",
 			type: "POST",
-			data: ({i_id: $(this).attr('value') }),
+			data: ({i_id: date }),
 			dataType: "text",
 			success: func_success_repeat_dates
 		});
+	});
+	$('body').on('click','.addEvent', function(){
+		$(location).attr('href','createArticlePage.php?parameters='+date);
 	});
 	function func_success_repeat_dates(data)
 	{
