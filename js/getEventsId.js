7cf65f6cab1c3b18867e630e9f2e02be69c2b795
EgorRozhko <59769440+EgorRozhko@users.noreply.github.com>
2020-12-13 15:02:50 +0300

    Update getEventsId.js

--- js/getEventsId.js
+++ js/getEventsId.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
 	$('body').on('click','.oneOfEvents',function(){
 		var item_id = this.id;				
-		$('body').append('<div id="eventTitle"><>'+$(this).text()+'</div>');
+		$('body').append('<div id="eventTitle"><p>'+$(this).text()+'</p><img id="closeEvent" src="images/close.png"></div>');
+		$('#eventTitle').css('margin-top', $('header').height());
 		$.ajax({
 			url: "php/ssd.php",
 			type: "POST",
 			data: ({i_id: item_id}),
 			dataType: "text",
 			success: function(data){
-				$('map, .bigCalendar, .monument, .obla2, #listOfEvents, .obla').remove();
+				$('area, .bigCalendar, .monument, .obla2, #listOfEvents, .obla').remove();
 				$('.activeYear').removeClass('activeYear');
 				$('#map').attr('src','images/blockedObla.png');
 				$('#map-container').append(data);
@@ -26,6 +27,25 @@ $(document).ready(function(){
 			data: ({i_id: item_id}),
 			dataType: "text",
 			success: function(data){ $('body').append(data); }
+		});
+	});
+
+	$('body').on('click', '#closeEvent', function(){
+		$('#map').attr('src','images/secondObla.png');
+		$('.obla, #eventTitle').remove();
+		$.ajax({
+			url: 'php/show_monuments.php',
+			dataType: 'text',
+			type: 'POST',
+			data: ({ secondInclude: 1 }),
+			success: function(data){ $('#map-container').append(data); }
+		});
+		$.ajax({
+			url: 'php/showArea.php',
+			dataType: 'text',
+			type: 'POST',
+			data: ({ secondInclude: 1 }),
+			success: function(data){ $('map').append(data); }
 		});
 	});
 
