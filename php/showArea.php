8f698f44b4f037c63490f4dfbd7973e56d1aaf36
EgorRozhko <59769440+EgorRozhko@users.noreply.github.com>
2020-12-13 15:01:08 +0300

    Update showArea.php

--- php/showArea.php
+++ php/showArea.php
@@ -1,7 +1,7 @@
 <?php
-	include_once 'mysql/connect.php';
+	if (isset($_POST['secondInclude'])) include_once "../mysql/connect.php";
+	else include_once "mysql/connect.php";
 	$selectAllFromPlacesQuery = mysqli_query($connection, 'SELECT * FROM places');
-	include_once 'php/arrayRegions.php';
 	while ($row = mysqli_fetch_array($selectAllFromPlacesQuery)) 
 	{
 		echo "<area id='".$row['id_place']."' shape='poly' coords='".$row['coordsMapImage']."'>";
