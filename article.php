ff521ebf54ff2f0c362ec4373d55a4bac8fd0a18
EgorRozhko <59769440+EgorRozhko@users.noreply.github.com>
2020-12-15 16:17:17 +0300

    Update article.php

--- article.php
+++ article.php
@@ -7,27 +7,20 @@
 		{
 			include_once 'mysql/connect.php';
 			$parameters = explode('-', $_GET['a']);
-			if ($parameters[2] == 'a') {
+			if ($parameters[2] == 'a') 
+			{
 				$query  = mysqli_query($connection,'SELECT * FROM events WHERE events_id='.$parameters[0]);
 				$result = mysqli_fetch_array($query);
-				echo 
-			'<div id="content">
-				<span id="onMain" onclick="backToMainPage()">На главную</span>'.
-				$result['full_description'];
-				if ($parameters[1] == 't') 
-				{ 
-					if ($result['active'] == 0)	echo "<p class='actionButton'><span id='relizeArticle'>Опубликовать статью</span></p>
-						<input id='articleId' type='hidden' value=".$parameters[0].">";
-					echo "<p class='actionButton'><span id='removeArticle'>Удалить статью</span></p>";
-				}
-				echo '</div>';
 			}
-			else{
+			else
+			{
 				$query  = mysqli_query($connection,'SELECT * FROM monuments WHERE monuments_id='.$parameters[0]);
 				$result = mysqli_fetch_array($query);
-				echo 
-			'<div id="content">
-				<span id="onMain" onclick="backToMainPage()">На главную</span>'.
+			}
+			echo 
+			'	<input type="hidden" id="title" value="'.$result['title'].'">
+				<div id="content">
+					<span id="onMain" onclick="backToMainPage()">На главную</span>'.
 				$result['full_description'];
 				if ($parameters[1] == 't') 
 				{ 
@@ -38,7 +31,6 @@
 				echo '</div>';
 				mysqli_free_result($query);
 				mysqli_close($connection);
-			}
 		}
 		function jsFiles(){ 
 			echo "
